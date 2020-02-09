76dd4889cb1acb7631f742bd54440db36b80004b
Misha Balin <m.balin@icloud.com>
2020-02-09 21:59:25 +0300

    fix

--- packages/components/src/Button/Button.jsx
+++ packages/components/src/Button/Button.jsx
@@ -1,2 +1,6 @@
 import React from 'react';
-export default ({ children, ...rest }) => <button {...rest}>{children}sc</button>;
+export default ({ children, ...rest }) => (
+  <button type="button" {...rest}>
+    {children}sc
+  </button>
+);
